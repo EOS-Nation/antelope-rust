9dda79881657703d28aa2bd18fe210bc9f638961
Denis Carriere <carriere.denis@gmail.com>
2023-01-19 16:31:43 -0500

    Add tests for check

--- src/check.rs
+++ src/check.rs
@@ -9,13 +9,28 @@
 /// use antelope::check::check;
 ///
 /// #[test]
-/// #[should_panic]
 /// check(true, "This should not panic");
 ///
 /// #[test]
+/// #[should_panic]
 /// check(false, "This should panic");
 /// ```
-#[inline]
 pub fn check(predicate: bool, message: &str) {
     assert!(predicate, "{}", message)
+}
+
+#[cfg(test)]
+mod check_tests {
+    use super::*;
+
+    #[test]
+    fn test_check_true() {
+        check(true, "This should not panic")
+    }
+
+    #[test]
+    #[should_panic(expected = "This should panic")]
+    fn test_check_false() {
+        check(false, "This should panic");
+    }
 }
