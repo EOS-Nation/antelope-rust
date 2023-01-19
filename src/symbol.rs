ec4aa73597848137b4a86d8509714a9d9b9f65c1
YaroShkvorets <shkvorets@gmail.com>
2023-01-19 14:27:56 -0500

    symbol code refactoring - Exposed symbol and check modules in the main module, so you can do `antelope::SymbolCode` instead of `antelope::symbol::SymbolCode` - Removed Bool conversions since CDT doesn’t have them. Better use `is_valid()` - Replaced PartialEq, PartialOrd, Ord traits implementations with default derives - Made `SymbolCode::value` field private like in CDT. To access it we use `raw()` - To init symcode changed `SymbolCode::new(“EOS”)` to `SymbolCode::from(“EOS”)` and `SymbolCode::from(542341)`. That seems to be the best practice in Rust libraries. `SymbolCode::new()` now returns default symbol code with `value=0` like in CDT - Add `#[must_use]` attribute for methods. - Add some tests

--- src/symbol.rs
+++ src/symbol.rs
@@ -2,7 +2,6 @@
 use std::cmp::{Ord, Ordering, PartialEq, PartialOrd};
 use std::convert::From;
 use std::fmt::{Display, Formatter, Result};
-use std::ops::Not;
 
 use crate::check;
 
@@ -19,13 +18,13 @@ use crate::check;
 /// ```
 /// use antelope::SymbolCode;
 ///
-/// let symcode = SymbolCode::new("FOO");
+/// let symcode = SymbolCode::from("FOO");
 /// assert_eq!(5197638, symcode.raw());
 /// assert_eq!(3, symcode.length());
 /// assert_eq!(true, symcode.is_valid());
 /// assert_eq!("FOO", symcode.to_string());
 /// ```
-#[derive(Eq, Copy, Clone, Debug)]
+#[derive(Eq, Copy, Clone, Debug, PartialEq, PartialOrd, Ord)]
 pub struct SymbolCode {
     /// The raw value of the symbol code
     ///
@@ -38,10 +37,10 @@ pub struct SymbolCode {
     /// ```
     /// use antelope::SymbolCode;
     ///
-    /// let symcode = SymbolCode::new("FOO");
-    /// assert_eq!(5197638, symcode.value);
+    /// let symcode = SymbolCode::from("FOO");
+    /// assert_eq!(5197638, symcode.raw());
     /// ```
-    pub value: u64,
+    value: u64,
 }
 
 impl SymbolCode {
@@ -56,9 +55,10 @@ impl SymbolCode {
     /// ```
     /// use antelope::SymbolCode;
     ///
-    /// let symcode = SymbolCode::new("FOO");
+    /// let symcode = SymbolCode::from("FOO");
     /// assert_eq!(5197638, symcode.raw());
     /// ```
+    #[must_use]
     pub fn raw(&self) -> u64 {
         self.value
     }
@@ -74,9 +74,10 @@ impl SymbolCode {
     /// ```
     /// use antelope::SymbolCode;
     ///
-    /// let len = SymbolCode::new("FOO").length();
+    /// let len = SymbolCode::from("FOO").length();
     /// assert_eq!(3, len);
     /// ```
+    #[must_use]
     pub fn length(&self) -> u32 {
         let mut sym: u64 = self.value;
         let mut len: u32 = 0;
@@ -97,9 +98,10 @@ impl SymbolCode {
     /// ```
     /// use antelope::SymbolCode;
     ///
-    /// let symcode = SymbolCode::new("FOO");
+    /// let symcode = SymbolCode::from("FOO");
     /// assert_eq!(true, symcode.is_valid());
     /// ```
+    #[must_use]
     pub fn is_valid(&self) -> bool {
         let mut sym: u64 = self.value;
         let mut i = 0;
@@ -134,10 +136,11 @@ impl SymbolCode {
     /// ```
     /// use antelope::SymbolCode;
     ///
-    /// let symcode = SymbolCode::new("FOO");
+    /// let symcode = SymbolCode::from("FOO");
     /// assert_eq!("FOO", symcode.to_string());
-    pub fn new(str: &str) -> SymbolCode {
-        SymbolCode::from(str)
+    #[must_use]
+    pub fn new() -> SymbolCode {
+        SymbolCode { value: 0 }
     }
 }
 
@@ -164,40 +167,8 @@ impl Display for SymbolCode {
     }
 }
 
-impl Not for SymbolCode {
-    type Output = bool;
-
-    /// Returns true if the symbol code is empty
-    ///
-    /// A symbol code is empty if it is equal to 0
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use antelope::SymbolCode;
-    ///
-    /// let symcode = SymbolCode::new("FOO");
-    /// assert_eq!(false, !symcode);
-    /// ```
-    fn not(self) -> bool {
-        self.value == 0
-    }
-}
-
-// impl Clone for SymbolCode {
-//     fn clone(&self) -> Self {
-//         SymbolCode { value: self.value }
-//     }
-// }
-
-impl From<u64> for SymbolCode {
-    fn from(value: u64) -> Self {
-        SymbolCode { value }
-    }
-}
-
-impl From<String> for SymbolCode {
-    fn from(str: String) -> Self {
+impl From<&str> for SymbolCode {
+    fn from(str: &str) -> Self {
         let mut value: u64 = 0;
         check(
             str.len() <= 7,
@@ -215,43 +186,9 @@ impl From<String> for SymbolCode {
     }
 }
 
-impl From<&str> for SymbolCode {
-    fn from(str: &str) -> Self {
-        SymbolCode::from(str.to_string())
-    }
-}
-
-impl PartialEq for SymbolCode {
-    fn eq(&self, other: &Self) -> bool {
-        self.value == other.value
-    }
-}
-
-impl PartialOrd for SymbolCode {
-    fn partial_cmp(&self, other: &SymbolCode) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for SymbolCode {
-    fn cmp(&self, other: &SymbolCode) -> Ordering {
-        self.value.cmp(&other.value)
-    }
-}
-
-impl From<SymbolCode> for bool {
-    fn from(_other: SymbolCode) -> bool {
-        _other.is_valid()
-    }
-}
-
-trait AsBool {
-    fn as_bool(&self) -> bool;
-}
-
-impl AsBool for SymbolCode {
-    fn as_bool(&self) -> bool {
-        self.is_valid()
+impl From<u64> for SymbolCode {
+    fn from(value: u64) -> Self {
+        SymbolCode { value }
     }
 }
 
@@ -261,166 +198,127 @@ impl AsRef<SymbolCode> for SymbolCode {
     }
 }
 
-#[test]
-fn test_as_ref() {
-    assert_eq!(5197638, SymbolCode::from("FOO").as_ref().value);
-    assert_eq!(5390658, SymbolCode::from("BAR").as_ref().value);
-}
 
-#[test]
-fn test_clone() {
-    SymbolCode::new("FOO").clone();
-}
-
-#[test]
-fn test_some_unwrap() {
-    let symcode = Some(SymbolCode::new("FOO"));
-}
-
-#[test]
-fn test_length() {
-    assert_eq!(1, SymbolCode::from("A").length());
-    assert_eq!(2, SymbolCode::from("AB").length());
-    assert_eq!(3, SymbolCode::from("ABC").length());
-    assert_eq!(4, SymbolCode::from("ABCD").length());
-    assert_eq!(5, SymbolCode::from("ABCDE").length());
-    assert_eq!(6, SymbolCode::from("ABCDEF").length());
-    assert_eq!(7, SymbolCode::from("ABCDEFG").length());
-}
-
-#[test]
-fn test_not() {
-    assert_eq!(true, SymbolCode::from(0).not());
-    assert_eq!(true, !SymbolCode::from(0));
-    assert_eq!(false, SymbolCode::from(5197638).not());
-    assert_eq!(false, !SymbolCode::from(5197638));
-}
-
-#[test]
-fn test_partial_eq() {
-    assert_eq!(true, SymbolCode::from(5197638) == SymbolCode::from(5197638));
-    assert_eq!(true, SymbolCode::from(0) != SymbolCode::from(5197638));
-    assert_eq!(false, SymbolCode::from(0) == SymbolCode::from(5197638));
-}
-
-#[test]
-fn test_partial_cmp() {
-    assert_eq!(true, SymbolCode::from(0) < SymbolCode::from(1));
-    assert_eq!(false, SymbolCode::from(3) < SymbolCode::from(2));
-}
-
-#[test]
-fn test_new() {
-    assert_eq!("FOO", SymbolCode::new("FOO").to_string());
-    assert_eq!(5197638, SymbolCode::new("FOO").raw());
-}
-
-#[test]
-fn test_fmt() {
-    assert_eq!("FOO", format!("{}", SymbolCode::new("FOO")));
-}
-
-#[test]
-fn test_println() {
-    println!("{}", SymbolCode::new("FOO"));
-}
-
-#[test]
-fn test_as_bool() {
-    assert_eq!(true, SymbolCode::from(5197638).as_bool());
-}
-
-#[test]
-fn test_into_bool() {
-    assert_eq!(true, SymbolCode::from(5197638).into());
-    assert_eq!(false, SymbolCode::from(0).into());
-    if SymbolCode::from(0).into() {
-        panic!("SymbolCode::from( 0 ) should be false");
+#[cfg(test)]
+mod symbol_code_tests {
+    use super::*;
+    #[test]
+    fn test_as_ref() {
+        assert_eq!(5197638, SymbolCode::from("FOO").as_ref().value);
+        assert_eq!(5390658, SymbolCode::from("BAR").as_ref().value);
     }
-}
 
-#[test]
-fn test_if_bool() {
-    if SymbolCode::from(0).into() {
-        panic!("SymbolCode::from( 0 ) should be false");
+    #[test]
+    fn test_clone() {
+        SymbolCode::from("FOO").clone();
     }
-}
 
-#[test]
-#[should_panic]
-fn test_if_bool_panic() {
-    if SymbolCode::from(5197638).into() {
-        panic!("SymbolCode::from( 5197638 ) should be true");
+    #[test]
+    fn test_length() {
+        assert_eq!(1, SymbolCode::from("A").length());
+        assert_eq!(2, SymbolCode::from("AB").length());
+        assert_eq!(3, SymbolCode::from("ABC").length());
+        assert_eq!(4, SymbolCode::from("ABCD").length());
+        assert_eq!(5, SymbolCode::from("ABCDE").length());
+        assert_eq!(6, SymbolCode::from("ABCDEF").length());
+        assert_eq!(7, SymbolCode::from("ABCDEFG").length());
     }
-}
 
-#[test]
-fn test_from() {
-    assert_eq!(0, SymbolCode::from(0).value);
-    assert_eq!(0, SymbolCode::from(0).raw());
-    assert_eq!(0, SymbolCode::from("".to_string()).raw());
-    assert_eq!(5197638, SymbolCode::from("FOO".to_string()).value);
-    assert_eq!(5197638, SymbolCode::from(5197638).raw());
-}
+    #[test]
+    fn test_partial_eq() {
+        assert_eq!(true, SymbolCode::from(5197638) == SymbolCode::from(5197638));
+        assert_eq!(true, SymbolCode::from(0) != SymbolCode::from(5197638));
+        assert_eq!(false, SymbolCode::from(0) == SymbolCode::from(5197638));
+        assert_eq!(true, SymbolCode::from(5197637) != SymbolCode::from(5197638));
+    }
 
-#[test]
-fn test_is_valid() {
-    assert_eq!(false, SymbolCode::from(0).is_valid());
-    assert_eq!(true, SymbolCode::from("FOO".to_string()).is_valid());
-}
+    #[test]
+    fn test_partial_cmp() {
+        assert_eq!(true, SymbolCode::from(0) < SymbolCode::from(1));
+        assert_eq!(false, SymbolCode::from(3) < SymbolCode::from(2));
+    }
 
-#[test]
-fn test_to_string() {
-    assert_eq!("FOO", SymbolCode::from(5197638).to_string());
-    assert_eq!("FOO", SymbolCode::from("FOO".to_string()).to_string());
-    assert_eq!(
-        "ABCDEFG",
-        SymbolCode::from("ABCDEFG".to_string()).to_string()
-    );
-}
+    #[test]
+    fn test_new() {
+        assert_eq!("FOO", SymbolCode::from("FOO").to_string());
+        assert_eq!(5197638, SymbolCode::from("FOO").raw());
+    }
 
-#[test]
-fn test_to_str() {
-    assert_eq!("FOO", SymbolCode::from("FOO").to_string());
-}
+    #[test]
+    fn test_fmt() {
+        assert_eq!("FOO", format!("{}", SymbolCode::from("FOO")));
+    }
 
-#[test]
-#[should_panic(expected = "string is too long to be a valid symbol_code")]
-fn test_from_string_long_panic_1() {
-    SymbolCode::from("ABCDEFGH".to_string());
-}
+    #[test]
+    fn test_println() {
+        println!("{}", SymbolCode::from("FOO"));
+    }
 
-#[test]
-#[should_panic(expected = "only uppercase letters allowed in symbol_code string")]
-fn test_from_string_letters_panic_1() {
-    SymbolCode::from("abc".to_string());
-}
+    #[test]
+    fn test_from() {
+        assert_eq!(0, SymbolCode::from(0).value);
+        assert_eq!(0, SymbolCode::from(0).raw());
+        assert_eq!(0, SymbolCode::from("").raw());
+        assert_eq!(5197638, SymbolCode::from("FOO").value);
+        assert_eq!(5197638, SymbolCode::from(5197638).raw());
+    }
 
-#[test]
-#[should_panic(expected = "only uppercase letters allowed in symbol_code string")]
-fn test_from_string_letters_panic_2() {
-    SymbolCode::from("123".to_string());
-}
+    #[test]
+    fn test_is_valid() {
+        assert_eq!(false, SymbolCode::new().is_valid());
+        assert_eq!(false, SymbolCode::from(0).is_valid());
+        assert_eq!(false, SymbolCode::from(std::u64::MAX).is_valid());
+        assert_eq!(true, SymbolCode::from(5197638).is_valid());
+        assert_eq!(true, SymbolCode::from("FOO").is_valid());
+    }
 
-#[test]
-fn test_symbol_code_ord() {
-    let symbol_code1 = SymbolCode::new("ABCDEFG");
-    let symbol_code2 = SymbolCode::new("ABCDEFH");
-    let symbol_code3 = SymbolCode::new("ABCDEF");
-    assert_eq!(symbol_code1.cmp(&symbol_code2), Ordering::Less);
-    assert_eq!(
-        symbol_code1.partial_cmp(&symbol_code2),
-        Some(Ordering::Less)
-    );
-    assert_eq!(symbol_code1.cmp(&symbol_code3), Ordering::Greater);
-    assert!(symbol_code1 > symbol_code3);
-    assert!(symbol_code3 < symbol_code2);
-}
+    #[test]
+    fn test_to_string() {
+        assert_eq!("FOO", SymbolCode::from(5197638).to_string());
+        assert_eq!("FOO", SymbolCode::from("FOO").to_string());
+        assert_eq!("ABCDEFG", SymbolCode::from("ABCDEFG").to_string());
+    }
 
-#[test]
-fn test_symbol_code_clone() {
-    let symbol_code = SymbolCode::new("ABCDEFG");
-    let cloned_symbol_code = symbol_code.clone();
-    assert_eq!(symbol_code, cloned_symbol_code);
-    assert_eq!(symbol_code, cloned_symbol_code);
+    #[test]
+    fn test_to_str() {
+        assert_eq!("FOO", SymbolCode::from("FOO").to_string());
+    }
+
+    #[test]
+    #[should_panic(expected = "string is too long to be a valid symbol_code")]
+    fn test_from_string_long_panic_1() {
+        SymbolCode::from("ABCDEFGH");
+    }
+
+    #[test]
+    #[should_panic(expected = "only uppercase letters allowed in symbol_code string")]
+    fn test_from_string_letters_panic_1() {
+        SymbolCode::from("abc");
+    }
+
+    #[test]
+    #[should_panic(expected = "only uppercase letters allowed in symbol_code string")]
+    fn test_from_string_letters_panic_2() {
+        SymbolCode::from("123");
+    }
+
+    #[test]
+    fn test_symbol_code_ord() {
+        let symbol_code1 = SymbolCode::from("ABCDEFG");
+        let symbol_code2 = SymbolCode::from("ABCDEFH");
+        let symbol_code3 = SymbolCode::from("ABCDEF");
+        assert!(symbol_code1 < symbol_code2);
+        assert!(symbol_code1 > symbol_code3);
+        assert!(symbol_code1 > symbol_code3);
+        assert!(symbol_code3 < symbol_code2);
+        assert!(symbol_code1 <= symbol_code1);
+        assert!(symbol_code1 >= symbol_code1);
+    }
+
+    #[test]
+    fn test_symbol_code_clone() {
+        let symbol_code = SymbolCode::from("ABCDEFG");
+        let cloned_symbol_code = symbol_code.clone();
+        assert_eq!(symbol_code, cloned_symbol_code);
+    }
 }
