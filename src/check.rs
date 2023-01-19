540e5d05de818e5538c7cdaed7814e2f90f8f516
Denis Carriere <carriere.denis@gmail.com>
2023-01-18 22:04:54 -0500

    update fmt

--- src/check.rs
+++ src/check.rs
@@ -1,4 +1,3 @@
-
 /// Checks that a condition is true, and panics if it is not.
 ///
 /// This is a convenience function for writing tests.
@@ -6,7 +5,7 @@
 /// # Examples
 /// ```
 /// use antelope::check::check;
-/// 
+///
 /// #[test]
 /// #[should_panic]
 /// check(true, "This should not panic");
