72f5b73e17b64d553eb8526f10aa0d5bbd373af2
YaroShkvorets <shkvorets@gmail.com>
2023-02-14 15:58:45 -0500

    implement ords with "type mismatch" panic

--- src/extended_asset.rs
+++ src/extended_asset.rs
@@ -1,5 +1,4 @@
-use crate::{Asset, ExtendedSymbol, Name};
-use std::cmp::{Ord, PartialEq, PartialOrd};
+use crate::{check, Asset, ExtendedSymbol, Name};
 /// The `ExtendedAsset` struct represents an extended asset
 ///
 /// Reference: <https://github.com/AntelopeIO/cdt/blob/main/libraries/eosiolib/core/eosio/asset.hpp>
@@ -12,7 +11,7 @@ use std::cmp::{Ord, PartialEq, PartialOrd};
 /// let ext_asset = ExtendedAsset::from_amount(10000, ExtendedSymbol::from_extended(Symbol::from("4,FOO"), Name::from("contract")));
 /// assert_eq!(10000, ext_asset.quantity.amount);
 /// ```
-#[derive(Eq, Copy, Clone, Debug, Default, Ord, PartialEq, PartialOrd)]
+#[derive(Eq, Copy, Clone, Debug, Default)]
 pub struct ExtendedAsset {
     /**
      * The asset
@@ -68,6 +67,27 @@ impl ExtendedAsset {
     }
 }
 
+impl std::cmp::PartialEq for ExtendedAsset {
+    fn eq(&self, other: &ExtendedAsset) -> bool {
+        check(self.contract == other.contract, "type mismatch");
+        self.quantity == other.quantity
+    }
+}
+
+impl std::cmp::PartialOrd for ExtendedAsset {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        check(self.contract == other.contract, "type mismatch");
+        self.quantity.partial_cmp(&other.quantity)
+    }
+}
+
+impl std::cmp::Ord for ExtendedAsset {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        check(self.contract == other.contract, "type mismatch");
+        self.quantity.cmp(&other.quantity)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -109,13 +129,12 @@ mod tests {
     fn test_ord() {
         let a = ExtendedAsset::from_amount(100, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
         let b = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
-        let c = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract2")));
-        let d = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
+        let c = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
 
         assert!(a < b);
         assert!(b > a);
-        assert!(b == d);
-        assert!(b != c);
+        assert!(b == c);
+        assert!(b != a);
     }
 
     #[test]
@@ -142,20 +161,42 @@ mod tests {
 
         assert!(a <= b);
     }
+    #[test]
+    #[should_panic(expected = "type mismatch")]
+    fn test_ord_panic4() {
+        let a = ExtendedAsset::from_amount(100, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract1")));
+        let b = ExtendedAsset::from_amount(100, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract2")));
 
-    // #[test]
-    // fn test_partial_ord() {
-    //     let a = ExtendedAsset::from_amount(100, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
-    //     let b = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
-    //     let c = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract2")));
+        assert!(a != b);
+    }
+    #[test]
+    #[should_panic(expected = "type mismatch")]
+    fn test_ord_panic5() {
+        let a = ExtendedAsset::from_amount(100, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract1")));
+        let b = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract2")));
 
-    //     assert!(a <= b);
-    //     assert!(b >= a);
-    //     assert!(b >= b);
-    //     assert!(b <= b);
-    //     assert!(b > c);
-    //     assert!(b >= c);
-    //     assert!(c < b);
-    //     assert!(c <= b);
-    // }
+        assert!(a < b);
+    }
+    #[test]
+    #[should_panic(expected = "type mismatch")]
+    fn test_ord_panic6() {
+        let a = ExtendedAsset::from_amount(100, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract1")));
+        let b = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract2")));
+
+        assert!(a <= b);
+    }
+
+    #[test]
+    fn test_partial_ord() {
+        let a = ExtendedAsset::from_amount(100, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
+        let b = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
+        let c = ExtendedAsset::from_amount(200, ExtendedSymbol::from_extended(Symbol::from("4,SYM"), Name::from("contract")));
+
+        assert!(a <= b);
+        assert!(b >= a);
+        assert!(b > a);
+        assert!(b >= c);
+        assert!(a < b);
+        assert!(c <= b);
+    }
 }
